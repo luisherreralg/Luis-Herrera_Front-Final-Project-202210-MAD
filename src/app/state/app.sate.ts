433e92977249eaa7fabb57520fc796dc370ace2d
luisherreralg <lherreralg@outlook.es>
2022-12-03 14:32:57 +0100

    🚧 Added the order reducer to the app.state

--- src/app/state/app.sate.ts
+++ src/app/state/app.sate.ts
@@ -1,11 +1,15 @@
 import { ActionReducerMap } from '@ngrx/store';
+import { OrderState } from '../interfaces/orders';
 import { SneakerState } from '../interfaces/sneakers';
+import { OrderReducer } from './order.reducer/order.reducer';
 import { SneakerReducer } from './sneaker.reducer/sneaker.reducer';
 
 export interface AppState {
   sneakers: SneakerState;
+  orders: OrderState;
 }
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
   sneakers: SneakerReducer,
+  orders: OrderReducer,
 };
