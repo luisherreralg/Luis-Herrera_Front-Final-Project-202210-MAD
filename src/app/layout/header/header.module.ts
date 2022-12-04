2b04ba846e27665221a405240651f3d7bb548ccb
luisherreralg <lherreralg@outlook.es>
2022-12-04 12:48:15 +0100

    🚧 Updated files

--- src/app/layout/header/header.module.ts
+++ src/app/layout/header/header.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderRoutingModule } from './header-routing.module';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [MenuComponent],
-  imports: [CommonModule, HeaderRoutingModule],
-  exports: [MenuComponent],
+  declarations: [MenuComponent, SearchBarComponent],
+  imports: [CommonModule, HeaderRoutingModule, ReactiveFormsModule],
+  exports: [MenuComponent, SearchBarComponent],
 })
 export class HeaderModule {}
