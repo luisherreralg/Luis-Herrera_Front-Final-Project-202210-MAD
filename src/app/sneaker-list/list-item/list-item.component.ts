cc3db4b90e62299697214120bb73d85004d87423
luisherreralg <lherreralg@outlook.es>
2022-12-04 17:09:18 +0100

    ✨ Created the item component from sneaker list

---
+++ src/app/sneaker-list/list-item/list-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Sneaker } from 'src/app/types/sneaker';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.css'],
+})
+export class ListItemComponent {
+  @Input() sneaker: Sneaker = {
+    id: 'Initial',
+    brand: 'Initial',
+    model: 'Initial',
+    size: [],
+    price: 0,
+    onSalePrice: 0,
+    onSale: false,
+    stock: 0,
+    gender: 'Initial',
+    images: [],
+  };
+}
