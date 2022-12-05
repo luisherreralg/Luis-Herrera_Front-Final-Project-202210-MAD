7cc0ab82747ab15b9dbe90e0079c2ee237476bbf
luisherreralg <lherreralg@outlook.es>
2022-12-05 23:24:38 +0100

    🐛 Cleaned a not used importation

--- src/app/multipage/multipage.component.spec.ts
+++ src/app/multipage/multipage.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { of } from 'rxjs';
