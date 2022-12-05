1de2128ac1c1ac71b392e11a9e35b4ccc2c687de
luisherreralg <lherreralg@outlook.es>
2022-12-05 23:48:57 +0100

    🐛 Fixed code smells

--- src/app/multipage/multipage.component.ts
+++ src/app/multipage/multipage.component.ts
@@ -4,7 +4,6 @@ import { Store } from '@ngrx/store';
 import { SneakersService } from '../services/sneakers.service';
 import { AppState } from '../state/app.sate';
 import { Sneaker } from '../types/sneaker';
-// import * as actions from '../state/sneaker.reducer/sneaker.action.creator';
 
 @Component({
   selector: 'app-multipage',
@@ -21,13 +20,11 @@ export class MultipageComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // this.title = this.route.snapshot.paramMap.get('title') as string;
     this.route.params.subscribe((params) => {
       this.title = params['title'];
-      this.title = ((this.title.charAt(0).toUpperCase() as string) +
-        this.title?.slice(1)) as string;
+      this.title = this.title.charAt(0).toUpperCase() + this.title?.slice(1);
 
-      this.service.searchSneakers(this.title as string).subscribe((data) => {
+      this.service.searchSneakers(this.title).subscribe((data) => {
         this.sneakers = data.sneakers;
       });
 
@@ -35,7 +32,7 @@ export class MultipageComponent implements OnInit {
         const filteredState = state.sneakers.sneakers.filter((sneaker) => {
           return sneaker.gender.includes(this.title);
         });
-        return (this.sneakers = filteredState);
+        this.sneakers = filteredState;
       });
     });
   }
