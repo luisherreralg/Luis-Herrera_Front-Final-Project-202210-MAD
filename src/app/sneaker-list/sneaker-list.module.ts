1145589e2bddf21848830eaf0070defe2c32747f
luisherreralg <lherreralg@outlook.es>
2022-12-05 20:40:08 +0100

    🚧 Fixed subroutes

--- src/app/sneaker-list/sneaker-list.module.ts
+++ src/app/sneaker-list/sneaker-list.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SneakerListComponent } from './sneaker-list.component';
 import { ListItemComponent } from './list-item/list-item.component';
-import { SneakerListRoutingModule } from './sneaker-list-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SneakerListComponent, ListItemComponent],
-  imports: [CommonModule, SneakerListRoutingModule],
+  imports: [CommonModule, RouterModule],
   exports: [SneakerListComponent],
 })
 export class SneakerListModule {}
