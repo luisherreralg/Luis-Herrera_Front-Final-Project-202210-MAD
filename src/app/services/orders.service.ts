7195c72b062b3a525639a0fb489220a122954b4c
luisherreralg <lherreralg@outlook.es>
2022-12-07 15:55:54 +0100

    ✨ Created the orders service

---
+++ src/app/services/orders.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order, ProtoOrder } from '../types/order';
+import { LocalStorageService } from './local-storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersService {
+  apiUrl: string;
+  constructor(
+    public storageService: LocalStorageService,
+    public http: HttpClient
+  ) {
+    this.apiUrl = 'http://localhost:7700/orders';
+  }
+
+  getOrders(): Observable<{ orders: Order[] }> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.storageService.getToken(),
+      }),
+    };
+
+    return this.http.get(this.apiUrl, httpOptions) as unknown as Observable<{
+      orders: Order[];
+    }>;
+  }
+
+  postOrder(newOrder: ProtoOrder, sneakerId: string): Observable<Order> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.storageService.getToken(),
+      }),
+    };
+
+    return this.http.post(
+      this.apiUrl + '/' + sneakerId,
+      newOrder,
+      httpOptions
+    ) as unknown as Observable<Order>;
+  }
+
+  deleteOrder(sneakerId: string): Observable<Order> {
+    const httpOptions = {
+      method: 'DELETE',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.storageService.getToken(),
+      }),
+    };
+
+    return this.http.delete(
+      this.apiUrl + '/' + sneakerId,
+      httpOptions
+    ) as unknown as Observable<Order>;
+  }
+}
