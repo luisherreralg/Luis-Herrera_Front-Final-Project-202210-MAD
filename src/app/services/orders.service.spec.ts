900de3051680affce6dc15159dcb14a7bc75055e
luisherreralg <lherreralg@outlook.es>
2022-12-07 15:56:07 +0100

    ✅ Tested the orders service

---
+++ src/app/services/orders.service.spec.ts
@@ -0,0 +1,85 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { Order, ProtoOrder } from '../types/order';
+import { OrdersService } from './orders.service';
+
+describe('OrdersService', () => {
+  const mockOrder: Order = {
+    size: [],
+    cartedItem: {
+      id: '',
+      brand: '',
+      model: '',
+      size: [],
+      price: 0,
+      onSalePrice: 0,
+      onSale: '',
+      stock: 0,
+      gender: '',
+      images: [],
+    },
+    cartedBy: {
+      id: '',
+      name: '',
+      surname: '',
+      email: '',
+      password: '',
+      role: 'user',
+    },
+    orderId: '',
+  };
+
+  let service: OrdersService;
+  let httpTest: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(OrdersService);
+    httpTest = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('When the getOrders method is invoked', () => {
+    it('should call to the HttpClient', () => {
+      service.getOrders().subscribe((resp) => {
+        expect(resp).not.toBeNull();
+        expect(JSON.stringify(resp)).toBe(JSON.stringify(mockOrder));
+      });
+      expect(httpTest).toBeTruthy();
+    });
+  });
+
+  describe('When the postOrder method is invoked', () => {
+    it('should call to the HttpClient', () => {
+      const newOrder: ProtoOrder = {
+        size: '35.5',
+        cartedItem: '',
+        cartedBy: '',
+      };
+
+      service.postOrder(newOrder, 'sneakerId').subscribe((resp) => {
+        expect(resp).not.toBeNull();
+        expect(JSON.stringify(resp)).toBe(JSON.stringify(newOrder));
+      });
+      expect(httpTest).toBeTruthy();
+    });
+  });
+
+  describe('When the deleteOrder method is invoked', () => {
+    it('should call to the HttpClient', () => {
+      service.deleteOrder('sneakerId').subscribe((resp) => {
+        expect(resp).not.toBeNull();
+        expect(JSON.stringify(resp)).toBe(JSON.stringify({}));
+      });
+      expect(httpTest).toBeTruthy();
+    });
+  });
+});
