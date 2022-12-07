a9b2fcf3cd9a5b22af7915743bca834717a8444b
luisherreralg <lherreralg@outlook.es>
2022-12-07 12:46:59 +0100

    ✅ Updated login-modal test

--- src/app/utils/modals/login-modal/login-modal.component.spec.ts
+++ src/app/utils/modals/login-modal/login-modal.component.spec.ts
@@ -1,4 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { of } from 'rxjs';
+import { UsersService } from 'src/app/services/users.service';
 
 import { LoginModalComponent } from './login-modal.component';
 
@@ -6,9 +14,22 @@ describe('LoginModalComponent', () => {
   let component: LoginModalComponent;
   let fixture: ComponentFixture<LoginModalComponent>;
 
+  const userServiceSpy = jasmine.createSpyObj('UsersService', [
+    'register',
+    'login',
+  ]);
+  userServiceSpy.login.and.returnValue(of({}));
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [LoginModalComponent],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: userServiceSpy,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginModalComponent);
@@ -20,7 +41,7 @@ describe('LoginModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('Given the hanlderLoginModalEvent, when its invoked', () => {
+  describe('Given the handlerLoginModalEvent, when its invoked', () => {
     it('it should call to the emit function when the button is clicked', () => {
       // spy on the output emitter
       spyOn(component.handlerLoginModal, 'emit');
@@ -34,5 +55,60 @@ describe('LoginModalComponent', () => {
       fixture.detectChanges();
       expect(component.handlerLoginModal.emit).toHaveBeenCalled();
     });
+  });
+
+  describe('Given the loginForm validators, when the form is assigned', () => {
+    it('should be false if the form data do not pass the validations', () => {
+      component.formLogin.setValue({
+        email: '',
+        password: 'password',
+      });
+
+      expect(component.formLogin.valid).toEqual(false);
+    });
+
+    it('should be true if the form data do not pass the validations', () => {
+      component.formLogin.setValue({
+        email: 'email@gmail.com',
+        password: 'password',
+      });
+
+      expect(component.formLogin.valid).toEqual(true);
+    });
+  });
+
+  describe('Given the loginHandler method, when its invoked ', () => {
+    it('should not call to the userService if the formData is not valid', fakeAsync(() => {
+      const formData = {
+        email: '',
+        password: 'password',
+      };
+      component.formLogin.setValue(formData);
+      component.loginHandler();
+
+      expect(component.invalidType).toEqual(true);
+      tick(3000);
+      expect(component.invalidType).toEqual(false);
+      expect(userServiceSpy.login).not.toHaveBeenCalled();
+    }));
+
+    it('should call to the userService if the formData is valid', fakeAsync(() => {
+      const formData = {
+        email: 'email@gmail.com',
+        password: 'password',
+      };
+
+      const spyLocalService = spyOn(component.storageService, 'saveToken');
+
+      component.formLogin.setValue(formData);
+      component.loginHandler();
+
+      expect(component.invalidCredentials).toEqual(true);
+      tick(3000);
+      expect(component.invalidCredentials).toEqual(false);
+
+      expect(userServiceSpy.login).toHaveBeenCalled();
+      expect(spyLocalService).toHaveBeenCalled();
+    }));
   });
 });
