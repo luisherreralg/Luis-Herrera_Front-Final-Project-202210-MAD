66f59309256116ae7eb2d98093daad87700aa64c
luisherreralg <lherreralg@outlook.es>
2022-12-08 21:22:07 +0100

    🚧 Improved searchbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { SneakersService } from '../services/sneakers.service';
 import { AppState } from '../state/app.state';
+import * as actions from '../state/sneaker.reducer/sneaker.action.creator';
 import { Sneaker } from '../types/sneaker';
 
 @Component({
@@ -10,17 +12,25 @@ import { Sneaker } from '../types/sneaker';
 export class HomeComponent implements OnInit {
   onSaleSneakers: Sneaker[] = [];
   sneakers: Sneaker[] = [];
-  constructor(public store: Store<AppState>) {}
+  constructor(
+    public store: Store<AppState>,
+    public sneakerService: SneakersService
+  ) {}
 
   ngOnInit(): void {
     this.store.subscribe((state) => {
       this.sneakers = state.sneakers.sneakers;
+
+      if (this.sneakers.length > 0) {
+        this.onSaleSneakers = this.sneakers.filter(
+          (sneaker) => sneaker.onSale === 'onSale'
+        );
+      }
     });
 
-    if (this.sneakers.length > 0) {
-      this.onSaleSneakers = this.sneakers.filter(
-        (sneaker) => sneaker.onSale === 'onSale'
-      );
-    }
+    this.sneakerService.getSneakers().subscribe((data) => {
+      this.sneakers = data.sneakers;
+      this.store.dispatch(actions.loadSneakers({ sneakers: this.sneakers }));
+    });
   }
 }
--- src/app/multipage/multipage.component.ts
+++ src/app/multipage/multipage.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { SneakersService } from '../services/sneakers.service';
 import { AppState } from '../state/app.state';
+import * as actions from '../state/sneaker.reducer/sneaker.action.creator';
 import { Sneaker } from '../types/sneaker';
 
 @Component({
@@ -20,20 +21,25 @@ export class MultipageComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.store.subscribe((state) => {
+      this.sneakers = state.sneakers.sneakers;
+    });
+
     this.route.params.subscribe((params) => {
       this.title = params['title'];
       this.title = this.title.charAt(0).toUpperCase() + this.title?.slice(1);
 
       this.service.searchSneakers(this.title).subscribe((data) => {
         this.sneakers = data.sneakers;
+        this.store.dispatch(actions.loadSneakers({ sneakers: this.sneakers }));
       });
 
-      this.store.subscribe((state) => {
-        const filteredState = state.sneakers.sneakers.filter((sneaker) => {
-          return sneaker.gender.includes(this.title);
-        });
-        this.sneakers = filteredState;
-      });
+      // this.store.subscribe((state) => {
+      //   const filteredState = state.sneakers.sneakers.filter((sneaker) => {
+      //     return sneaker.gender.includes(this.title);
+      //   });
+      //   this.sneakers = filteredState;
+      // });
     });
   }
 }
