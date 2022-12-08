bfe0e55b2cfc095c1f2b997b94fabc23f2119336
luisherreralg <lherreralg@outlook.es>
2022-12-08 13:21:09 +0100

    :white_check_mark: Updated cart-modal test

--- src/app/utils/modals/cart-modal/cart-modal.component.spec.ts
+++ src/app/utils/modals/cart-modal/cart-modal.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { Order } from 'src/app/types/order';
 import { mockInitialState, mockOrderInitialState } from '../../mocks/mocks';
 
 import { CartModalComponent } from './cart-modal.component';
@@ -23,5 +25,27 @@ describe('CartModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Given the handlerLoginModalEvent, when its invoked', () => {
+    it('should emit an event with the handlerCartModal event emitter', () => {
+      const spy = spyOn(component.handlerCartModal, 'emit');
+
+      component.handlerLoginModalEvent();
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('When the ngOnInit es invoked', () => {
+    it('should call to the orderServices', () => {
+      const spy = spyOn(component.orderService, 'getOrders').and.returnValue(
+        of({ orders: [] as Order[] })
+      );
+
+      component.ngOnInit();
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
