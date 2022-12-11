31f599c5f74c96750cd8449b808ba556b925da27
luisherreralg <lherreralg@outlook.es>
2022-12-11 12:37:43 +0100

    ✨ Created the admin component and module

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppComponent } from '../app.component';
+import { LocalStorageService } from '../services/local-storage.service';
+import { SneakersService } from '../services/sneakers.service';
+import { Sneaker } from '../types/sneaker';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  sneakers: Sneaker[] = [];
 
+  constructor(
+    public store: Store<AppComponent>,
+    public localStorageService: LocalStorageService,
+    public sneakersService: SneakersService
+  ) {}
+
+  ngOnInit(): void {
+    this.sneakersService.getSneakers().subscribe((sneakers) => {
+      this.sneakers = sneakers.sneakers;
+    });
+  }
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
-
-
+import { AdminListComponent } from './admin-list/admin-list.component';
 
 @NgModule({
-  declarations: [
-    AdminComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AdminComponent, AdminListComponent],
+  imports: [CommonModule],
+  exports: [AdminComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AdminModule { }
+export class AdminModule {}
