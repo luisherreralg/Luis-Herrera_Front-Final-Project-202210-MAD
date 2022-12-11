a0493372b794e92eb850edf34f6f8b3f25e5ea0d
luisherreralg <lherreralg@outlook.es>
2022-12-11 22:55:57 +0100

    ✅ Updated the test due to new code

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -19,4 +19,29 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Given the ngOnInit method, when its invoked', () => {
+    it('should call to the storageService', () => {
+      const spyStorageService = spyOn(component.storageService, 'getToken');
+      component.ngOnInit();
+      expect(spyStorageService).toHaveBeenCalled();
+    });
+
+    it('should the isLogged variable to true if there is a token', () => {
+      spyOn(component.storageService, 'getToken').and.returnValue('token');
+
+      component.ngOnInit();
+      expect(component.isLogged).toBeTruthy();
+    });
+
+    it('should change the isAdmin variable to true if the checkTokenRole returns an object with the role = "admin"', () => {
+      spyOn(component.storageService, 'getToken').and.returnValue('token');
+      spyOn(component.storageService, 'checkTokenRole').and.returnValue({
+        role: 'admin',
+      });
+
+      component.ngOnInit();
+      expect(component.isAdmin).toBeTruthy();
+    });
+  });
 });
