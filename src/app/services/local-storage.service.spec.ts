1b9855704addbf7499543410ef72728cea38b489
luisherreralg <lherreralg@outlook.es>
2022-12-11 23:24:46 +0100

    ✅ Updated failing tests

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -48,24 +48,6 @@ describe('LocalStorageService', () => {
     });
   });
 
-  describe('When checkTokenRole is invoked', () => {
-    it('should call to the localStorage.getItem method', () => {
-      spyOn(localStorage, 'getItem').and.returnValue(null);
-
-      service.checkTokenRole('token');
-      expect(localStorage.getItem).toHaveBeenCalled();
-    });
-
-    it('should return a role if there is an existing one saved before', () => {
-      spyOn(localStorage, 'getItem').and.returnValue('role');
-
-      service.checkTokenRole(
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-      );
-      expect(localStorage.getItem).toHaveBeenCalledWith('role');
-    });
-  });
-
   describe('When the saveSneakerId method is invoked', () => {
     it('should call to the localStorage.setItem method', () => {
       spyOn(localStorage, 'setItem');
@@ -81,6 +63,19 @@ describe('LocalStorageService', () => {
 
       service.deleteSneakerId();
       expect(localStorage.removeItem).toHaveBeenCalled();
+    });
+  });
+
+  describe('Given the checkTokenRole method, when its invoked', () => {
+    it('should return an object if its all correct', () => {
+      const result = service.checkTokenRole(
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+      );
+      expect(result).toEqual({
+        sub: '1234567890',
+        name: 'John Doe',
+        iat: 1516239022,
+      });
     });
   });
 });
