45b90364fbc8fcacb6eb1388764a3d94fa2fa93d
luisherreralg <lherreralg@outlook.es>
2022-12-11 22:50:56 +0100

    ✅ Tested the admin-list.component

--- src/app/admin/admin-list/admin-list.component.spec.ts
+++ src/app/admin/admin-list/admin-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { Sneaker } from 'src/app/types/sneaker';
 import { mockSneakersInitialState } from 'src/app/utils/mocks/mocks';
 
 import { AdminListComponent } from './admin-list.component';
@@ -24,5 +26,41 @@ describe('AdminListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Given the handleDeleteSneaker  method, when its invoked', () => {
+    it('should call to the sneakerServices', () => {
+      const spySneakersService = spyOn(
+        component.sneakerServices,
+        'deleteSneaker'
+      ).and.returnValue(of({} as Sneaker));
+
+      component.handleDeleteSneaker('sneakerID');
+      expect(spySneakersService).toHaveBeenCalled();
+    });
+  });
+
+  describe('Given the handleEditSneaker  method, when its invoked', () => {
+    it('should call to the localStorageService', () => {
+      const spySneakersService = spyOn(
+        component.localStorageService,
+        'saveSneakerId'
+      );
+
+      component.handleEditSneaker('sneakerID');
+      expect(spySneakersService).toHaveBeenCalled();
+    });
+  });
+
+  describe('Given the ngOnInit method, when its invoked', () => {
+    it('should call to the sneakerServices', () => {
+      const spySneakersService = spyOn(
+        component.sneakerServices,
+        'getSneakers'
+      ).and.returnValue(of({ sneakers: [] }));
+
+      component.ngOnInit();
+      expect(spySneakersService).toHaveBeenCalled();
+    });
   });
 });
