f009cc468443e47c766304fd81448c0231d9b82e
luisherreralg <lherreralg@outlook.es>
2022-12-11 23:53:37 +0100

    ✅ Updated test due to new code

--- src/app/utils/modals/admin-edit-modal/admin-edit-modal.component.spec.ts
+++ src/app/utils/modals/admin-edit-modal/admin-edit-modal.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { ProtoSneaker, Sneaker } from 'src/app/types/sneaker';
 import { mockSneakersInitialState } from '../../mocks/mocks';
 
 import { AdminEditModalComponent } from './admin-edit-modal.component';
@@ -24,5 +26,53 @@ describe('AdminEditModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Given the handlerAdminEditModalEvent method, when its invoked', () => {
+    it('should call to the modalService', () => {
+      spyOn(component.modalService, 'adminEditModal');
+      component.handlerAdminEditModalEvent();
+      expect(component.modalService.adminEditModal).toHaveBeenCalled();
+    });
+  });
+
+  describe('Given the handleEditSneaker, when its invoked', () => {
+    it('should call to the sneakerService and store dispatcher', () => {
+      const formData = {
+        brand: 'test',
+        model: 'test',
+        onSale: 'onSale',
+        price: 0,
+        onSalePrice: 0,
+        stock: 0,
+        gender: 'gender',
+      };
+
+      component.formEditSneaker.setValue(formData);
+      spyOn(component.sneakerService, 'patchSneaker').and.returnValue(
+        of({} as Sneaker)
+      );
+      spyOn(component.store, 'dispatch');
+
+      component.handleEditSneaker();
+      expect(component.sneakerService.patchSneaker).toHaveBeenCalled();
+      expect(component.store.dispatch).toHaveBeenCalled();
+    });
+  });
+
+  describe('Given the ngOnInit method, when its invoked', () => {
+    it('should call to the localStorageService and sneakerService', () => {
+      const spyLocalStorageService = spyOn(
+        component.sneakerService,
+        'getSneaker'
+      ).and.returnValue(of({ sneaker: {} as Sneaker }));
+      const spySneakerService = spyOn(
+        component.localStorageService,
+        'getSneakerId'
+      );
+      component.ngOnInit();
+      expect(spyLocalStorageService).toHaveBeenCalled();
+      expect(spySneakerService).toHaveBeenCalled();
+    });
   });
 });
