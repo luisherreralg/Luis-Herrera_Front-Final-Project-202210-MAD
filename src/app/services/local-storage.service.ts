01d53cc23f683841e03e12274b4e7bef2209a2de
luisherreralg <lherreralg@outlook.es>
2022-12-11 10:56:31 +0100

    🚧 Added a new service to decode the jwt tokens

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -19,4 +19,13 @@ export class LocalStorageService {
     }
     return token;
   }
+
+  checkTokenRole(token: string) {
+    if (!token) {
+      return;
+    }
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+  }
 }
