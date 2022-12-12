f87fcf6c417c02674f167eee93ff69dac59244f0
luisherreralg <lherreralg@outlook.es>
2022-12-12 17:40:52 +0100

    🚧 Fast test of firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,53 @@ import { AppState } from './state/app.state';
 import * as actions from './state/sneaker.reducer/sneaker.action.creator';
 import { Sneaker } from './types/sneaker';
 
+// ! IMPORTACION DE FIREBASE
+import {
+  Storage,
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+} from '@angular/fire/storage';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  // ! FIREBASE
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  uploadImage($event: any) {
+    const file = $event.target.files[0];
+    console.log(file);
+
+    const imgRef = ref(this.storage, 'sneakers/' + file.name);
+
+    uploadBytes(imgRef, file)
+      .then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+  }
+
+  getImages() {
+    const imgRef = ref(this.storage, 'sneakers');
+    console.log(imgRef);
+
+    listAll(imgRef)
+      .then(async (res) => {
+        console.log(res);
+
+        for (const item of res.items) {
+          const url = await getDownloadURL(item);
+          console.log(url);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  // ? FIREBASE
+
   sneakers: Sneaker[] = [];
 
   subscription: Subscription;
@@ -30,7 +72,8 @@ export class AppComponent implements OnInit {
     public store: Store<AppState>,
     public sneakerService: SneakersService,
     public modalService: ModalHandlerService,
-    public pathService: WebLocationService
+    public pathService: WebLocationService,
+    public storage: Storage
   ) {
     this.subscription = this.modalService.getLoginModal().subscribe((value) => {
       this.loginModal = value;
