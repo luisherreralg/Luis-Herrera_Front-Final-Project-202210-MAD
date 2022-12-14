91d178437c1ccfba0e5c95552be31d13f0bdd840
luisherreralg <lherreralg@outlook.es>
2022-12-14 21:34:00 +0100

    🐛 Cleaned console.logs

--- src/app/services/sneakers.service.ts
+++ src/app/services/sneakers.service.ts
@@ -68,11 +68,6 @@ export class SneakersService {
   }
 
   postSneaker(sneaker: ProtoSneaker): Observable<{ sneaker: Sneaker }> {
-    console.log(
-      '🚀 ~ file: sneakers.service.ts:71 ~ SneakersService ~ postSneaker ~ sneaker',
-      sneaker
-    );
-
     const httpOptions = {
       method: 'POST',
       headers: new HttpHeaders({
